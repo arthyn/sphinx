e595f1d2f196fe7f2b33394fe4187944192f5770
Hunter Miller <hunter@hmiller.dev>
2023-01-04 22:59:53 -0600

    updated kelvin and fixing error message

--- ui/src/components/ErrorMessage.tsx
+++ ui/src/components/ErrorMessage.tsx
@@ -1,16 +1,16 @@
 import cn from 'classnames';
 import React from 'react';
-import { LiteralUnion, RegisterOptions, useFormContext } from 'react-hook-form';
+import { FieldValues, LiteralUnion, RegisterOptions, useFormContext } from 'react-hook-form';
 
 type Validation = LiteralUnion<keyof RegisterOptions, string>;
 
-interface ErrorMessageProps<T> {
+interface ErrorMessageProps<T extends FieldValues> {
   field: keyof T;
   messages: Partial<Record<Validation, string>>;
   className?: string;
 }
 
-export function ErrorMessage<T>({ field, messages, className }: ErrorMessageProps<T>) {
+export function ErrorMessage<T extends FieldValues>({ field, messages, className }: ErrorMessageProps<T>) {
   const { formState } = useFormContext<T>();
   const error = formState.errors[field];
 
