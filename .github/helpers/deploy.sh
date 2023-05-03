5efa67f7bc6291881ef3dd075321bd9b38b1205d
Hunter Miller <hunter@hmiller.dev>
2023-05-02 22:23:17 -0500

    ops: remove mount for now

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -21,7 +21,6 @@ git clone --depth 1 git@github.com:urbit/urbit.git $urbit_repo -b '$URBIT_REPO_T
 landscape_repo=$(mktemp --dry-run /tmp/repo.landscape.XXXXXXXXX)
 git clone --depth 1 --branch master git@github.com:tloncorp/landscape.git $landscape_repo
 cd /home/kaladin
-curl -s --data '\''{"source":{"dojo":"+hood/mount %'$desk'"},"sink":{"app":"hood"}}'\'' http://localhost:12321
 rsync -avL --delete $urbit_repo/pkg/base-dev/ '$folder'
 rsync -avL $landscape_repo/desk-dev/ '$folder'
 rsync -avL $source_repo/desk/ '$folder'
